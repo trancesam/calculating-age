fe0399a4addca78b98ac00ef634d2eefbfbd6658
trancesam <89720020+trancesam@users.noreply.github.com>
2021-12-08 11:47:09 +0530

    initial commit

---
+++ #exercise- calculating age.py
@@ -0,0 +1,7 @@
+#exercise- calculating age
+year=input('In which year you were born?')
+present=input('Insert the present year.')
+age=int(present)-int(year)
+age1=str(age)
+print('Your age is {}' .format(age))
+#print('Your age is ' +str(age))
